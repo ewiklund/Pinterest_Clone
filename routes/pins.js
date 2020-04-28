0fe309acbcc454c5381529d7b19f1d1edf04b8be
ewiklund <38758177+ewiklund@users.noreply.github.com>
2020-04-28 15:55:53 +0100

    updated code

--- routes/pins.js
+++ routes/pins.js
@@ -1,24 +1,46 @@
-const path = require("path");
-const Pin = require("../models/pin");
-
-
-
+let path = require("path");
+let Pin = require("../models/pin");
 
 
 module.exports = function(app) {
+
+  app.get("/pins/pin-search", function(req, res, next) {
+    Pin.find({"title" : {"$regex": req.query['search'], "$options": "1"}},function(err, pins) {
+      res.render("./pins/index", {pins: pins});
+    })
+  })
+
+  app.get("/pins/saved-pins", function(req, res, next) {
+    Pin.find({"isSave" : true}, function(err, pins) {
+      res.render("./pins/index", {pins: pins});
+    })
+  })
+
+  app.get("/pins/pin-save/:id", function(req, res, next) {
+    Pin.findOne({_id: req.params.id}, function(err, foundPin) {
+      if(foundPin) {
+        foundPin.isSave = !(foundPin.isSave);
+
+        foundPin.save(function(err) {
+          if(err) return next(err);
+          res.redirect("/pins/details/"+foundPin._id);
+        })
+      }
+    })
+  })
   app.route("/pins/create")
      .get(function(req, res, next) {
        res.render("pins/create");
      })
      .post(function(req, res, next) {
-       const pin = new Pin();
+       let pin = new Pin();
        pin.title = req.body.title;
        pin.desc = req.body.desc;
        pin.username = req.body.username;
        pin.isSave = false;
 
        if(!req.files)
-        return json("error");
+        res.json("error");
 
         let sampleFile = req.files.sampleFile;
         let fileName = Math.random().toString(26).slice(2) + ".jpg";
@@ -36,9 +58,37 @@ module.exports = function(app) {
         })
     })
 
+    app.route("/pins/edit/:id")
+       .get(function(req, res, next) {
+         Pin.findOne({_id: req.params.id}), function(err, foundPin) {
+            res.render("pins/edit", {pin: foundPin});
+         }
+       })
+       .post(function(req, res, next) {
+         Pin.findOne({_id: req.params.id}), function(err, foundPin) {
+           if(foundPin) {
+             if(req.body.title) foundPin.title = req.body.title;
+             if(req.body.desc) foundPin.desc = req.body.desc;
+
+             foundPin.save(function(err) {
+               if(err) return next(err);
+               res.redirect("/pins/details" + foundPin._id);
+             })
+           }
+
+       }
+     })
+
     app.get("/pins/index", function(req, res, next) {
       Pin.find({}, function(err, pins) {
         res.render("pins/index", {pins: pins});
+      })
+    })
+
+    app.get("/pins/details/:id", function(req, res, next) {
+      Pin.findOne({_id: req.params.id})
+      .exec(function(err, foundPin) {
+        res.render("./pins/details",{pin: foundPin});
       })
     })
 
@@ -48,5 +98,5 @@ module.exports = function(app) {
            res.redirect("/pins/index");
          })
       })
-    
+
 }
